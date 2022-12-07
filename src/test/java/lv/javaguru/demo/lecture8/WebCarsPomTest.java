d846f7a282414619d9e332715b5fff22243b4c6e
K Mezavilks <kristaps.mezavilks@ui.com>
2022-12-07 20:47:18 +0200

    Adding lecture 8 materials with POM structure

---
+++ src/test/java/lv/javaguru/demo/lecture8/WebCarsPomTest.java
@@ -0,0 +1,43 @@
+package lv.javaguru.demo.lecture8;
+
+import lv.javaguru.demo.lecture7.DriverFactory;
+import lv.javaguru.demo.lecture8.pages.CarsPage;
+import lv.javaguru.demo.lecture8.pages.MainPage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class WebCarsPomTest {
+
+    private static WebDriver driver;
+
+    @Before
+    public void setUpDriver() {
+        driver = DriverFactory.getDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void demoPomTest() throws InterruptedException {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open();
+        mainPage.openCarsPage();
+
+        CarsPage carsPage = new CarsPage(driver);
+        carsPage.inputMinPrice(6000);
+        carsPage.inputMaxPrice(10000);
+        carsPage.selectColor("Balta");
+
+        Thread.sleep(10000);
+    }
+
+    @After
+    public void tearDownDriver() {
+        driver.close();
+        driver.quit();
+    }
+}
